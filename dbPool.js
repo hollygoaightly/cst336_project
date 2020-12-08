050ca7c451bb26c2799c3370bdc2516c1fd0fac9
Marianna Petrovich <bowierule@gmail.com>
2020-12-08 06:22:11 +0000

    Fixed connection pooling

--- dbPool.js
+++ dbPool.js
@@ -1,15 +1,11 @@
 // database
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'qc0w3lq0xdqs5ny3',
     password: 'ea3ex48nw4hd4v3q',
     database: 'sn1qvahom0zodcij'
 });
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-});
 
-module.exports = connection;
+module.exports = pool;
